fa183095e912cbb8789ac7fa889f2fc8205bd144
tonsiena <meowearmuff@gmail.com>
2025-07-17 19:26:19 +0400

    redesign

--- script.js
+++ script.js
@@ -1,119 +1,131 @@
-import { e3, o4, q5, collections, tabs, fetchAccount, fetchNumbers, sliceAddress, display} from './core.js';
+import { e3, o4, q5, collections, tabs, fetchAccount, fetchNumbers, sliceAddress, display, cfetch} from './core.js';
 
 var   filtrableItems = [];
-var   tabsLoaded     = false;
 var   isgridview     = true;
 var   currentAddress = null;
-const divContainer   = q5(".tabs");
-const divContnent    = q5('.content');
-const tabAddress     = q5("#tab-address");
-const inputFilter    = q5("#search-filter");
-const searchHeader   = q5(".search-header");
-const inputSearch    = q5("#search-address");
-const buttonGridView = q5("#grid-view-button");
-const buttonSearch   = q5("#search-address-button");
+var   isAscending    = true;
 
-buttonSearch.onclick = async (e) => {
-        var res = await fetchAccount(inputSearch.value)
+const Elements = {
+    content:      q5(".content"),
+    sortList:     q5("#sort-list-button"),
+    changeView:   q5("#changeView"),
+    inputSearch:  q5(".search-input"),
+    container:    q5("#container"),
+    buttonSearch: q5("#search-button"),
+    filt:         q5(".filt"),
+    Nav: {
+        next:     q5("#nav-next"),
+        prev:     q5("#nav-prev"),
+        current:  q5("#nav-current"),
+    }
+};
+
+ Elements.buttonSearch.onclick = async (e) => {
+        var res = await fetchAccount(Elements.inputSearch.value)
         if (res) setCurrentAddress(res.address);
 }
-
-inputSearch.oninput = () => display(buttonSearch, inputSearch.value.trim() ? 'flex' : 'none');
-
-const loadTabs = () => {
- tabs.forEach(tab => {
-    if (tab.section != undefined) {
-        divContainer.appendChild(o4("div", { className: "section" }));
-    } else divContainer.appendChild(
-        o4("p", {
-            className: "tab border",
-            id: tab.id,
-            innerHTML: tab.text,
-            onclick: () => openModal(tab.id)
-        })
-    );
-});
-tabsLoaded = true;
+Elements.inputSearch.oninput = () => {
+     display(Elements.buttonSearch, Elements.inputSearch.value.trim() ? 'flex' : 'none');
+     display(Elements.filt, Elements.inputSearch.value.trim() ? 'none' : 'flex');
 }
 
-function filterItems() {
-    const searchValue = inputFilter.value.toLowerCase();
-    divContnent.innerHTML = '';
-    filtrableItems.forEach(item => {
-        if (item.name.toLowerCase().includes(searchValue)) divContnent.appendChild(item.element);
-    });
-}
-
-async function openModal(collection) {
-    if (!currentAddress) {
-        display(searchHeader, "none");
-        return;
-    }
-
-    const collectionAddress = collections[collection.split("tab-")[1]];
-    const { nft_items: items } = await fetchNumbers(currentAddress, collectionAddress);
-    divContnent.innerHTML = '';
-    filtrableItems = [];
-
-    if (items?.length) {
-        display(divContnent, isgridview ? "grid" : "flex");
-        items.forEach(({ previews, metadata }) => {
-            const div = e3("div");
-            div.className = isgridview ? "collections-grid-item" : "collections-item";
-            div.appendChild(o4("img", { src: previews[1].url }));
-            div.appendChild(o4("span", { textContent: metadata.name }));
-            divContnent.appendChild(div);
-            filtrableItems.push({ element: div, name: metadata.name });
-        });
-        inputFilter.oninput = filterItems;
-        display(searchHeader, "flex");
-    }
-}
-
-buttonGridView.onclick = () => {
+Elements.changeView.onclick = () => {
     isgridview = !isgridview;
-    divContnent.style.cssText = isgridview
+    Elements.content.style.cssText = isgridview
         ? "display: grid;"
         : "display: flex; flex-direction: column;";
     document.querySelectorAll(".collections-item, .collections-grid-item")
         .forEach(i => i.className = isgridview ? "collections-grid-item" : "collections-item");
-    console.log(isgridview);
-    buttonGridView.textContent = isgridview ?  "List" : "Grid"
-};
+    };
 
 
 document.addEventListener('keydown', async (event) => {
-    if (event.ctrlKey && event.key === '/' && inputSearch.value.length != 0) {
-        var res = await fetchAccount(inputSearch.value)
+    if (event.ctrlKey && event.key === '/' && Elements.inputSearch.value.length != 0) {
+        var res = await fetchAccount(Elements.inputSearch.value)
         if (res) setCurrentAddress(res.address);
+             display(Elements.buttonSearch, 'none');
+     display(Elements.filt, 'flex');
         event.preventDefault();
     }
 });
 
 const setCurrentAddress = (address) => {
-    inputSearch.value = "";
-    divContnent.innerHTML = '';
-    display(searchHeader, "none");
-    display(buttonSearch, 'none');
+    Elements.inputSearch.value = "";
+    Elements.content.innerHTML = '';
     if(!address) return;
     currentAddress = address;
-    tabAddress.textContent = sliceAddress(new TonWeb.Address(currentAddress).toString(true, true, false, false), 16);
-    if(!tabsLoaded) loadTabs();
+ loadCollections();
 }
 
-var isAscending = true;
-q5("#sort-list-button").onclick = () => {
-    isAscending = !isAscending; 
-    sortItems(); 
+var collectionsList = { address: [], names:[] }
+
+
+var CollectionCore = {
+    offset: 0, limit: 300,
+    load: async function () {
+        return await cfetch(
+            "https://toncenter.com/api/v3/nft/items?owner_address=" + currentAddress +
+            "&limit=" + this.limit + "&offset=" + this.offset);
+    }
 };
+
+async function loadCollections(prop) {
+    var { nft_items: items, metadata: meta, address_book: book } = await CollectionCore.load();
+
+    if (items?.length) {
+        Elements.container.innerHTML = `<ul class="content"></ul>`
+        Elements.content = q5(".content"),
+        collectionsList = { address: [], names:[] }
+        filtrableItems = [];
+        display(Elements.content, isgridview ? "grid" : "flex");
+        items.forEach(({ address, content, collection, collection_address }) => {
+            const li = o4("li", { classList: "border " + (isgridview ? "collections-grid-item" : "collections-item")});
+            if (collection) {
+                var metadata = meta[collection_address];
+                var token_info = metadata.token_info[0];
+                var isvalid = token_info.valid;
+
+                if (collection.address && collection_address) {
+                    if (!collectionsList.address.includes(collection.address)) {
+                        if (isvalid) {
+                            var icon = meta[collection_address].token_info[0].image;
+                            if (icon) {
+                                collectionsList.address.push(collection.address)
+                                collectionsList.names.push(token_info.name)
+                                li.appendChild(o4("img", { width: 100, height: 100, loading:"lazy",src:  meta[collection_address].token_info[0].image }));
+                                li.appendChild(o4("span", { textContent: token_info.name }));
+                                Elements.content.appendChild(li);
+                                filtrableItems.push({ element: li, name: token_info.name });
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }else Elements.container.innerHTML = `<p class="empty-page">nothing was found</p>`
+}
+
+Elements.sortList.onclick = () => sortItems(); 
 
 
 function sortItems() {
+    isAscending = !isAscending; 
+    filtrableItems.sort((a, b) => isAscending ? a.name.length - b.name.length : b.name.length - a.name.length);
 
-    filtrableItems.sort((a, b) => 
-        isAscending ? a.name.length - b.name.length : b.name.length - a.name.length
-    );
+    Elements.content.innerHTML = '';
+    filtrableItems.forEach(item => Elements.content.appendChild(item.element));
+}
 
-    divContnent.innerHTML = '';
-    filtrableItems.forEach(item => divContnent.appendChild(item.element));
+Elements.Nav.next.onclick = () => {
+    CollectionCore.offset += CollectionCore.limit;
+    Elements.Nav.current.textContent = CollectionCore.offset + " - " + (CollectionCore.offset +  CollectionCore.limit);
+    loadCollections();
+}
+
+Elements.Nav.prev.onclick = () => {
+    if (CollectionCore.offset -  CollectionCore.limit >= 0) {
+        Elements.Nav.current.textContent = (CollectionCore.offset -  CollectionCore.limit) + " - " + CollectionCore.offset;
+        CollectionCore.offset -=  CollectionCore.limit;
+        loadCollections();
+    }
 }
