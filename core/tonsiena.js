2eeeece4c451917b909364f8f273ff55e5b403dc
tonsiena <meowearmuff@gmail.com>
2025-08-07 23:11:55 +0400

    *

--- core/tonsiena.js
+++ core/tonsiena.js
@@ -15,6 +15,10 @@ export const Siena = {
         if (string.length < length) return string;
         return string.slice(0, length / 2) + "..." + string.slice(-length / 2);
     },
+      sliceUrl: (string, length) => {
+        if (string.length < length) return string;
+        return string.slice(0, length / 2) + "..." + string.slice(-length / 2);
+    },
     isNumbers: (RequestData) => RequestData?.collection === "0:0E41DC1DC3C9067ED24248580E12B3359818D83DEE0304FABCF80845EAFAFDB2",
     isDomains: (RequestData) => RequestData?.collection === "0:80D78A35F955A14B679FAA887FF4CD5BFC0F43B4A4EEA2A7E6927F3701B273C2",
     isMobile: () => {
--- nft/script.js
+++ nft/script.js
@@ -1,38 +1,73 @@
 import { Siena } from "../core/tonsiena.js"
 
-var $ = jQuery;
+const $ = jQuery, src = "append", to = "appendTo"; 
 
-var loadItem = (ts, item, todo) => $.getJSON(`https://${ts}toncenter.com/api/v3/nft/items?address=${item}&limit=1&offset=0`)
-    .then(todo).catch(error => console.log(error));
+var send = (ts, item, todo) => $.getJSON(`https://${ts}toncenter.com/api/v3/nft/items?address=${item}&limit=1&offset=0`).then(todo).catch(e => console.log(e));
+
+const $el = (selector, name) => $(name ? `<${selector} class=" ${name}">` : `<${selector}>`)
+const $p = (name, text) => $el("p", name).text(text)
+const $span = (name) => $el("span", name);
+const $div = (name) => $el("div", name);
+const $li = (name) => $el("li", name)
+const $img = (name) => $el("img", name);
+const $a = (href, text) => $(`<a href="${href}">${text}</a>`)
 
 
 $(document).ready(() => {
-    var params = Siena.getUrlParams();
-    loadItem("", params.get("item"), ({ nft_items: [{ address, collection_address, owner_address, content, collection }], metadata, addressbook }) => {
 
-        var metaitem = metadata[address];
-        var token_info = metaitem.token_info[0];
+    const startParam = WebApp.initDataUnsafe?.start_param || "";
+  const params = Siena.getUrlParams();
+  var nftAddress = startParam || params.get("item");
+  
+  if (startParam.startsWith("nft_"))
+     nftAddress = startParam.replace("nft_", "");
+
+    if(!nftAddress) return;
+    send("", nftAddress, ({ nft_items: [{ address, collection_address, owner_address, content, collection }], metadata, addressbook }) => {
+
+        var metaitem = metadata[address], { token_info } = metaitem, info = token_info[0], { extra } = info;
         var $content = $(".content");
-        var $preview = $('<img class="item-preview">')
-            .attr('src', token_info.extra?._image_small || '../icons/nopic.png')
-            .attr('alt', 'Preview');
 
-        $preview.appendTo($content);
-        console.log(metadata)
+        var $iCard = $div('i-card') [src] (
+            $img("preview")
+                .attr('src', extra?._image_small || '../icons/nopic.png').attr('alt', 'Preview')
+            ) [to] ($content);
 
-        var addItem = (key, value) => value && $("<li class='infoli'>").append($("<h2>").text(key), $("<h5>").text(value)).appendTo($content);
-        addItem("name", token_info.name);
-        addItem("description", token_info.description);
+        var link = (key, val) => 
+            $p(`i-link`) [src] (
+                $span('link-desc').text(key), 
+                $a("https://tonviewer.com/" + val, Siena.sliceAddress(val, 24)
+            ).addClass("link"));
 
-        addItem("address", address);
-        addItem("collection", collection_address);
-        addItem("owner", owner_address);
-        addItem("content uri", content.uri)
-        addItem("collection content uri", collection.collection_content.uri)
-        addItem("image", token_info.image);
-        addItem("small image", token_info.extra?._image_small);
-        addItem("medium image", token_info.extra?._image_medium);
-        addItem("big image", token_info.extra?._image_big);
+        $div('card-desc') [src] (
+            link("item",       address), 
+            link("owner",      owner_address),
+            link("collection", collection_address)
+        ) [to] ($iCard)
+       
+        $p('links') [src] (
+            $a(`https://getgems.io/nft/${address}`,  "getgems"),
+            $a(`https://tonscan.org/nft/${address}`, "tonscan"),
+            $a(`https://tonviewer.com/${address}`,   "tonviewer")
+        ) [to] ($content);
+        
+        $p("name-desc") [src] ($span("item-name").text(info.name), $span("item-desc").text(info.description)) [to] ($content);
 
+        [["nft index", info.nft_index],
+        ["content uri", content.uri],
+        ["collection content uri", collection.collection_content.uri],
+        ["image", info.image],
+        ["small image", extra?._image_small],
+        ["medium image", extra?._image_medium],
+        ["big image", extra?._image_big]]
+        .forEach(i => i[1] && $li('list') [src] (
+            $p(null, i[0]),
+            $a(i[1], i[1])
+        ) [to] ($content));
+   
+        $p('metadata') [src] (
+            $p('metadata-header', "metadata"),
+            $p('metadata-code', JSON.stringify(metadata[address], null, 2))
+        ) [to] ($content);
     })
 })
