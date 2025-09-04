f496f84c06cdaa6e22747f03beb761c54efe0efe
tonsiena <meowearmuff@gmail.com>
2025-09-04 14:46:03 +0400

    global changes

--- gifts/script.js
+++ gifts/script.js
@@ -1,35 +1,30 @@
-let models = [], URLS = [];
-var gifts;
-let isAscending = false;
+let gifts, isAscending = false;
 $.get('list.json', function (data) {
   gifts = data;
-  $(generateGiftList);
-}).fail(function (error) {
-  console.error('Ошибка:', error);
-});
+  generateGiftList();
+}).fail(err => console.error('Ошибка:', err));
 
 function generateGiftList() {
   const $ul = $('#giftList').empty();
-  $.each(gifts, (index, gift) => {
+  gifts.forEach((gift, i) => {
     var $lit = $li();
-    $lit[src]($createTileImage(index));
-    $lit[src](`<p><span class="giftname">${gift.name}</span>
-                  <span class="giftid">${gift.id} #${index += 1}</span></p>`);
+    $lit[src](`${createTileImage(i)}
+              <p><span class="giftname">${gift.name}</span>
+              <span class="mcount">${gift.id} TOTAL:${gift.count} IS:${gift.supply}</span>
+              <a class="giftaddress" target="_blank" href="https://getgems.io/collection/${gift.address}">${gift.address}</a></p>`);
+    $lit.find('.giftaddress').click(e => e.stopPropagation());
 
     $lit.on('click', () => {
       const encodedData = encodeURIComponent(gift.name);
       window.location.href = `/tilesheet/?gift=${encodedData}`;
     });
-    $lit[src](`<span class="mcount">${gift.count}</span`)[to]($ul);
+    $lit[src](`<span class="giftid">${i + 1}</span`)[to]($ul);
   });
 }
 
-function $createTileImage(tileIndex) {
-  const pos = Math.floor(tileIndex * 64 / 512);
-  const col = tileIndex % 8 * 64;
-  const row = pos * 64;
-  const $img = $(`<img width="64" height="64" style="object-fit:none; transform:scale(0.5); object-position:-${col}px -${row}px; height=32px; width=32px;" src="/tilesheet/gifts.png">`);
-  return $img;
+function createTileImage(i) {
+  const col = (i % 8) * 64, row = Math.floor(i / 8) * 64;
+  return `<img width="64" height="64" style="object-fit:none; transform:scale(0.5); object-position:-${col}px -${row}px;" src="/tilesheet/gifts.png">`;
 }
 
 
@@ -37,23 +32,30 @@ $(document).ready(function () {
   $('#filterInput').on('input', function () {
     const filterValue = $(this).val().toLowerCase();
     $('#giftList li').each(function () {
-      const giftText = $(this).text().toLowerCase();
-      $(this).toggle(giftText.includes(filterValue));
+      $(this).toggle($(this).text().toLowerCase().includes(filterValue));
     });
   });
+
+  const $tabs = $('<div class="tabs">').prependTo('body');
+
+  $('<p>', { class: 'tab', text: 'Все' }).click(() => filterList('all')).appendTo($tabs);
+
+  [49, 50, 52, 55, 56, 58, 60, 62, 65, 70, 80, 90, 99, 100]
+    .forEach(count => 
+      $('<p>', { class: 'tab', text: count })
+      .click(() => filterList(count)).appendTo($tabs));
+
+  $('#az-za-list-sort-button').click(() => {
+    const $list = $('#giftList');
+    const $items = $('ul li span.giftname').get().sort((a, b) => {
+      const [textA, textB] = [a.textContent, b.textContent].map(t => t.toLowerCase());
+      return isAscending ? textA.localeCompare(textB) : textB.localeCompare(textA);
+    });
+    $list.empty().append($items.map(item => $(item).parent().parent()));
+    isAscending = !isAscending;
+    $('#az-za-list-sort-button').text(isAscending ? "A-Z" : "Z-A");
+  });
 });
-
-const counts = [49, 50, 52, 55, 56, 58, 60, 62, 65, 70, 80, 90, 99, 100];
-
-const $buttonContainer = $('<div class="tabs">');
-
-$('<p>', { class: 'tab', text: 'Все', click: () => filterList('all') }).appendTo($buttonContainer);
-
-counts.forEach(count => {
-  $('<p>', { class: 'tab', text: `${count}`, click: () => filterList(count) }).appendTo($buttonContainer);
-});
-
-$('body').prepend($buttonContainer);
 
 function filterList(count) {
   $('#filterInput').val('');
@@ -66,15 +68,3 @@ function filterList(count) {
     });
   }
 }
-$('#az-za-list-sort-button').click(() => {
-  const $list = $('#giftList');
-  const $items = $('ul li span.giftname').get();
-  $items.sort((a, b) => {
-      const textA = a.textContent.toLowerCase();
-      const textB = b.textContent.toLowerCase();
-      return isAscending ? textA.localeCompare(textB) : textB.localeCompare(textA);
-    });
-    $list.empty().append($items.map(item => $(item).parent().parent()));
-    isAscending = !isAscending;
-    $('#az-za-list-sort-button').text(isAscending ? "A-Z" : "Z-A");
-});
