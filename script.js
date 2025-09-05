3b25ce724e914cce978e4572f27700626a03306b
tonsiena <meowearmuff@gmail.com>
2025-09-06 00:07:09 +0400

    mainPage

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     const openNewTab = (url) => window.open(url, '_blank');
 
-    $('.list-item').click(function () {
+    $('.list-item').click(function (e) {
+        if ($(e.target).closest('a').length) return;
+
         var url = $(this).data('url');
         if (url) openNewTab(url);
     });
